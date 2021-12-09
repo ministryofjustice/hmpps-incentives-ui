8b2366f8f1bd53e73e66e9533740639f375da778
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2021-12-09 15:05:58 +0000

    Added config for HMPPS Prison API

--- server/config.ts
+++ server/config.ts
@@ -58,6 +58,15 @@ export default {
       systemClientId: get('SYSTEM_CLIENT_ID', 'clientid', requiredInProduction),
       systemClientSecret: get('SYSTEM_CLIENT_SECRET', 'clientsecret', requiredInProduction),
     },
+    hmppsPrisonApi: {
+      url: get('HMPPS_PRISON_API_URL', 'http://localhost:8080/api', requiredInProduction),
+      externalUrl: get('HMPPS_PRISON_API_EXTERNAL_URL', get('HMPPS_PRISON_API_URL', 'http://localhost:8080/api')),
+      timeout: {
+        response: Number(get('HMPPS_PRISON_API_TIMEOUT_RESPONSE', 10000)),
+        deadline: Number(get('HMPPS_PRISON_API_TIMEOUT_DEADLINE', 10000)),
+      },
+      agent: new AgentConfig(),
+    },
     tokenVerification: {
       url: get('TOKEN_VERIFICATION_API_URL', 'http://localhost:8100', requiredInProduction),
       timeout: {

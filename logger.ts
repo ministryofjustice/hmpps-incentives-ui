6194d7db7960583e1aacf6f1821272c3110b29f2
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-01-07 15:09:42 +0000

    Fixed case of HMPPS and UI in project name

--- logger.ts
+++ logger.ts
@@ -3,6 +3,6 @@ import bunyanFormat from 'bunyan-format'
 
 const formatOut = bunyanFormat({ outputMode: 'short', color: true })
 
-const logger = bunyan.createLogger({ name: 'Hmpps Incentives Ui', stream: formatOut, level: 'debug' })
+const logger = bunyan.createLogger({ name: 'HMPPS Incentives UI', stream: formatOut, level: 'debug' })
 
 export default logger

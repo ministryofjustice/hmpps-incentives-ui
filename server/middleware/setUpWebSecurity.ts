83f4fb9c55ba015b0e9f9fb83fe5cb1f07bceba1
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-02-15 11:32:39 +0000

    Middleware to generate CSP nonce
    See Content Security Policy docs:
    
    https://content-security-policy.com/nonce/

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -1,4 +1,5 @@
-import express, { Router } from 'express'
+import crypto from 'crypto'
+import express, { Router, Request, Response } from 'express'
 import helmet from 'helmet'
 
 export default function setUpWebSecurity(): Router {
@@ -7,13 +8,22 @@ export default function setUpWebSecurity(): Router {
   // Secure code best practice - see:
   // 1. https://expressjs.com/en/advanced/best-practice-security.html,
   // 2. https://www.npmjs.com/package/helmet
+  router.use((req, res, next) => {
+    res.locals.cspNonce = crypto.randomBytes(16).toString('hex')
+    next()
+  })
   router.use(
     helmet({
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ["'self'"],
           // Hash allows inline script pulled in from https://github.com/alphagov/govuk-frontend/blob/master/src/govuk/template.njk
-          scriptSrc: ["'self'", 'code.jquery.com', "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='"],
+          scriptSrc: [
+            "'self'",
+            'code.jquery.com',
+            "'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='",
+            (req: Request, res: Response) => `'nonce-${res.locals.cspNonce}'`,
+          ],
           styleSrc: ["'self'", 'code.jquery.com'],
           fontSrc: ["'self'"],
         },

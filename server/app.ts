bf5474f32684c9f78d881b1fbe065491e98690c6
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-02-23 15:56:31 +0000

    INC-249: Middleware refactoring
    Removed duplication between real express app and test express app.
    
    The test express app should be as similar as possible to the real expess
    app (the one determining how users' traffic is handled). This should
    reduce chances of tests running against a different version of the routing
    as before.
    
    In addition, now to add a route only one file needs to change instead of
    two (easy to miss)

--- server/app.ts
+++ server/app.ts
@@ -3,13 +3,9 @@ import express from 'express'
 import path from 'path'
 import createError from 'http-errors'
 
-import homeRoutes from './routes/home'
-import incentivesTableRoutes from './routes/incentivesTable'
-import selectLocationRoutes from './routes/selectLocation'
-import prisonerImagesRoutes from './routes/prisonerImages'
+import allRoutes from './routes/all'
 import nunjucksSetup from './utils/nunjucksSetup'
 import errorHandler from './errorHandler'
-import standardRouter from './routes/standardRouter'
 import type UserService from './services/userService'
 
 import setUpWebSession from './middleware/setUpWebSession'
@@ -19,7 +15,6 @@ import setUpAuthentication from './middleware/setUpAuthentication'
 import setUpHealthChecks from './middleware/setUpHealthChecks'
 import setUpWebRequestParsing from './middleware/setupRequestParsing'
 import authorisationMiddleware from './middleware/authorisationMiddleware'
-import imageRouter from './routes/imageRouter'
 
 export default function createApp(userService: UserService): express.Application {
   const app = express()
@@ -38,10 +33,7 @@ export default function createApp(userService: UserService): express.Application
   app.use(authorisationMiddleware())
 
   // App routes
-  app.use('/select-location', selectLocationRoutes(standardRouter(userService)))
-  app.use('/incentive-summary/:locationPrefix', incentivesTableRoutes(standardRouter(userService)))
-  app.use('/prisoner-images/:imageId.jpeg', prisonerImagesRoutes(imageRouter()))
-  app.use('/', homeRoutes(standardRouter(userService)))
+  app.use('/', allRoutes(userService))
 
   app.use((req, res, next) => next(createError(404, 'Not found')))
   app.use(errorHandler(process.env.NODE_ENV === 'production'))

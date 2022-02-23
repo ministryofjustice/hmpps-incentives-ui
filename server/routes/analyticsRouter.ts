ee63a12ed292f5b73fd1ae52623373886d15d6c6
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-02-23 21:48:42 +0000

    INC-247: Mockup behaviour entry graphs

--- server/routes/analyticsRouter.ts
+++ server/routes/analyticsRouter.ts
@@ -1,13 +1,21 @@
 import type { RequestHandler, Router } from 'express'
 
 import asyncMiddleware from '../middleware/asyncMiddleware'
+import AnalyticsService from '../services/analyticsService'
 
 export default function routes(router: Router): Router {
   const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/behaviour-entries', async (req, res) => {
+    const analyticsService = new AnalyticsService()
+
+    const behaviourEntries = await analyticsService.getBehaviourEntriesByLocation('????')
+    const prisonersWithEntries = await analyticsService.getPrisonersWithEntriesByLocation('????')
+
     res.render('pages/analyticsBehaviourEntries', {
       lastUpdated: new Date(),
+      behaviourEntries,
+      prisonersWithEntries,
     })
   })
 

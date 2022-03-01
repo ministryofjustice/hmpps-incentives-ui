a86a24ddfbe206cd87e28f869d0903347a6ca223
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-01 12:29:59 +0000

    INC-270: Link Sentry.io reports to application release git hash

--- server/middleware/setUpSentryIo.ts
+++ server/middleware/setUpSentryIo.ts
@@ -2,12 +2,14 @@ import type { Express, RequestHandler, ErrorRequestHandler } from 'express'
 import * as Sentry from '@sentry/node'
 
 import config from '../config'
+import applicationVersion from '../applicationVersion'
 
 export function setUpSentryRequestHandler(app: Express): void {
   if (config.sentry.dsn) {
     Sentry.init({
       dsn: config.sentry.dsn,
       environment: config.environment,
+      release: applicationVersion.gitRef,
     })
     app.use(
       Sentry.Handlers.requestHandler({

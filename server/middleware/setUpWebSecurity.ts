1e9b6745e25774392d1478d5f75f87f222f8256a
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-08 22:07:35 +0000

    INC-328: Change cross-origin embedder policy to allow loading of JQuery UI assets

--- server/middleware/setUpWebSecurity.ts
+++ server/middleware/setUpWebSecurity.ts
@@ -29,6 +29,7 @@ export default function setUpWebSecurity(): Router {
           imgSrc: ["'self'", 'https://www.google-analytics.com'],
         },
       },
+      crossOriginEmbedderPolicy: true,
     })
   )
 

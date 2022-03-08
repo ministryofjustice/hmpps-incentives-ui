ddff30963efeece622cd62e0e3701131141c53a3
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-08 21:43:46 +0000

    INC-327: Perform integration tests on analytics pages …fixing mocked user caseload response from NOMIS user roles api (the real one does not return an array)

---
+++ integration_tests/integration/analytics.spec.ts
@@ -0,0 +1,58 @@
+import Page from '../pages/page'
+import HomePage from '../pages/home'
+import AnalyticsIndex from '../pages/analyticsIndex'
+import AnalyticsBehaviourEntries from '../pages/analyticsBehaviourEntries'
+import AnalyticsIncentiveLevels from '../pages/analyticsIncentiveLevels'
+
+context('Analytics', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+
+    cy.signIn()
+
+    const homePage = Page.verifyOnPage(HomePage)
+    homePage.viewAnalyticsLink().click()
+  })
+
+  it('users see analytics index page with cards', () => {
+    const expectedCards = ['See behaviour entries', 'See incentive levels']
+
+    const page = Page.verifyOnPage(AnalyticsIndex)
+    page.cards.each((card, index) => {
+      const link = card.find('.card__link')
+      expect(link.text().trim()).to.equal(expectedCards[index])
+    })
+  })
+
+  it('users see behaviour entry analytics', () => {
+    const indexPage = Page.verifyOnPage(AnalyticsIndex)
+    indexPage.cards.eq(0).find('a').click()
+    const page = Page.verifyOnPage(AnalyticsBehaviourEntries)
+
+    page.entriesByLocation.first().then(totalsRow => {
+      const location = totalsRow.find('td:first-child').text()
+      expect(location).to.contain('All')
+      expect(location).to.contain('361')
+    })
+
+    page.prisonersWithEntriesByLocation.first().then(totalsRow => {
+      const location = totalsRow.find('td:first-child').text()
+      expect(location).to.contain('All')
+      expect(location).to.contain('1,379')
+    })
+  })
+
+  it('users see incentive levels analytocs', () => {
+    const indexPage = Page.verifyOnPage(AnalyticsIndex)
+    indexPage.cards.eq(1).find('a').click()
+    const page = Page.verifyOnPage(AnalyticsIncentiveLevels)
+
+    page.incentivesByLocation.first().then(totalsRow => {
+      const location = totalsRow.find('td:first-child').text()
+      expect(location).to.contain('All')
+      expect(location).to.contain('359')
+    })
+  })
+})

826015f6d99c55aab9d0b1e851689eb7b8e7aac3
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-14 22:10:28 +0000

    INC-289: Create a simple wrapper around S3 SDK …which will act as a data client

---
+++ server/data/s3Client.ts
@@ -0,0 +1,81 @@
+import type { Readable } from 'stream'
+
+import { S3Client as Client, GetObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3'
+
+import logger from '../../logger'
+
+type S3ClientConfig = {
+  bucket: string
+  accessKeyId?: string
+  secretAccessKey?: string
+  endpoint?: string
+}
+
+type ObjectInfo = {
+  key: string
+  modified: Date
+}
+
+export default class S3Client {
+  bucket: string
+
+  s3: Client
+
+  constructor({ bucket, endpoint, accessKeyId, secretAccessKey }: S3ClientConfig) {
+    this.bucket = bucket
+    const credentials =
+      accessKeyId && secretAccessKey
+        ? {
+            accessKeyId,
+            secretAccessKey,
+          }
+        : undefined
+    this.s3 = new Client({
+      region: 'eu-west-2',
+      credentials,
+      endpoint,
+      forcePathStyle: true,
+    })
+  }
+
+  async getObject(key: string): Promise<string> {
+    logger.debug(`S3 Client getting object "${key}"`)
+    const command = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    })
+    const response = await this.s3.send(command)
+    const readableBody = response.Body as Readable
+    const chunks: Uint8Array[] = []
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const chunk of readableBody) {
+      chunks.push(chunk)
+    }
+    return Buffer.concat(chunks).toString('utf8')
+  }
+
+  async listObjects(prefix?: string): Promise<ObjectInfo[]> {
+    logger.debug(`S3 Client listing objects within "${prefix ?? '/'}"`)
+    let nextToken: string | undefined
+    const objects: ObjectInfo[] = []
+    do {
+      const command = new ListObjectsV2Command({
+        Bucket: this.bucket,
+        Prefix: prefix,
+        ContinuationToken: nextToken,
+      })
+      // eslint-disable-next-line no-await-in-loop
+      const response = await this.s3.send(command)
+      const contents = response.Contents || []
+      const results = contents.map(object => {
+        return {
+          key: object.Key,
+          modified: object.LastModified,
+        }
+      })
+      nextToken = response.NextContinuationToken
+      objects.push(...results)
+    } while (nextToken)
+    return objects
+  }
+}

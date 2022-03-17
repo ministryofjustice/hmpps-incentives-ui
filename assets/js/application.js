52db78d9a2960caed42fb44efd38ff8402f39b34
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-17 16:19:47 +0000

    INC-374: Track opening of guidance boxes with GA events

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,2 +1,15 @@
 window.GOVUKFrontend.initAll()
 window.MOJFrontend.initAll()
+
+$(() => {
+  // track opening/closing of <details> when labelled with GA category
+  $('.govuk-details').on('toggle', e => {
+    const $details = $(e.target)
+    const eventCategory = $details.data('ga-category')
+    const eventAction = $details.prop('open') ? 'opened' : 'closed'
+    const eventLabel = $details.data('ga-label') || ''
+    if (eventCategory && typeof ga === 'function') {
+      ga('send', 'event', eventCategory, eventAction, eventLabel)
+    }
+  })
+})

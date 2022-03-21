5b455d4e3e12e86ca21ad30de6973cb9e994227e
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-21 17:48:30 +0000

    INC-289: Include script to redact & truncate source data from Analytical Platform for inclusion as testing data

---
+++ bin/truncate-source-table.ts
@@ -0,0 +1,89 @@
+#!/usr/bin/env npx ts-node
+import fs from 'fs'
+import path from 'path'
+
+import { type Table, TableType } from '../server/services/analyticsServiceTypes'
+
+const tableTypes: ReadonlyArray<string> = Object.keys(TableType).sort()
+
+function printHelpAndExit() {
+  const scriptName = path.basename(process.argv[1])
+  const tableTypeOptions = tableTypes.map(t => `"${t}"`).join(' | ')
+  process.stderr.write(`Usage: ./${scriptName} [${tableTypeOptions}] [path-to-source-JSON]\n`)
+  process.exit(1)
+}
+
+const [chosenTableType, sourcePath] = process.argv.slice(2, 4)
+if (process.argv.length !== 4 || !tableTypes.includes(chosenTableType)) {
+  printHelpAndExit()
+}
+const destPath = path.resolve(
+  __dirname,
+  `../server/testData/s3Bucket/${TableType[chosenTableType]}/${path.basename(sourcePath)}`
+)
+
+let columnsToKeep: string[]
+let rowFilter: (rowIndex: string, table: Table) => boolean
+if (chosenTableType === 'behaviourEntries') {
+  columnsToKeep = ['prison', 'wing', 'positives', 'negatives']
+  rowFilter = () => true
+} else if (chosenTableType === 'incentiveLevels') {
+  columnsToKeep = ['prison', 'wing', 'incentive', 'characteristic', 'charac_group']
+  const characteristicsToKeep: ReadonlySet<string> = new Set(['ethnic_group', 'age_group_10yr'])
+  rowFilter = (rowIndex, table) => characteristicsToKeep.has(table.characteristic[rowIndex] as string)
+} else {
+  throw new Error(`Not implemented for: ${chosenTableType}`)
+}
+
+process.stderr.write('Reading source table…\n')
+const source = fs.readFileSync(sourcePath, { encoding: 'utf8' })
+process.stderr.write('Parsing source table…\n')
+const sourceTable = JSON.parse(source) as Table
+
+process.stderr.write('Deleting unnecessary columns…\n')
+Object.keys(sourceTable)
+  .filter(column => !columnsToKeep.includes(column))
+  .forEach(columnToDelete => delete sourceTable[columnToDelete])
+
+const prisonMap: ReadonlyMap<string, string> = new Map([
+  ['HHI', 'MDI'],
+  ['ACI', 'BWI'],
+])
+process.stderr.write('Selecting and renaming prisons…\n')
+const prisonColumn = sourceTable.prison as Record<string, string>
+const newPrisonColumn: Record<string, string> = Object.fromEntries(
+  Object.entries<string>(prisonColumn)
+    .map(([rowIndex, prison]) => {
+      const newPrison = prisonMap.get(prison)
+      if (newPrison) {
+        return [rowIndex, newPrison]
+      }
+      return null
+    })
+    .filter(entry => entry)
+)
+sourceTable.prison = newPrisonColumn
+
+process.stderr.write('Filtering rows…\n')
+const rowIndicesToKeep = Object.keys(newPrisonColumn).filter(rowIndex => rowFilter(rowIndex, sourceTable))
+process.stderr.write('Shuffling rows…\n')
+for (let i = rowIndicesToKeep.length - 1; i > 0; i -= 1) {
+  const j: number = Math.floor(Math.random() * (i + 1))
+  ;[rowIndicesToKeep[i], rowIndicesToKeep[j]] = [rowIndicesToKeep[j], rowIndicesToKeep[i]]
+}
+
+process.stderr.write('Constructing output table…\n')
+const destTable: typeof sourceTable = Object.fromEntries(columnsToKeep.map(column => [column, {}]))
+rowIndicesToKeep.forEach((rowIndex, newRowIndex) => {
+  columnsToKeep.forEach(column => {
+    destTable[column][newRowIndex.toString()] = sourceTable[column][rowIndex]
+  })
+})
+let dest = JSON.stringify(destTable)
+columnsToKeep.forEach(column => {
+  dest = dest.replace(`"${column}":`, `\n  "${column}":`)
+})
+dest = dest.slice(0, dest.length - 1)
+dest += '\n}\n'
+fs.writeFileSync(destPath, dest, { encoding: 'utf8' })
+process.stderr.write(`Saved output table to ${destPath}.\n`)

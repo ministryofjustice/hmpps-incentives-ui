c22fb6655c5952af17f940908650eb65ed3d3f54
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-21 14:19:42 +0000

    INC-289: Make table names/types global constants

--- server/testData/s3Bucket.ts
+++ server/testData/s3Bucket.ts
@@ -6,32 +6,33 @@ import { GetObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3'
 import type { GetObjectOutput, ListObjectsV2Output } from '@aws-sdk/client-s3'
 
 import type S3Client from '../data/s3Client'
+import type { TableType } from '../services/analyticsServiceTypes'
 
-export function mockAppS3ClientResponse(s3Client: jest.Mocked<S3Client>, tableName: string) {
+export function mockAppS3ClientResponse(s3Client: jest.Mocked<S3Client>, tableType: TableType) {
   s3Client.listObjects.mockResolvedValue([
     {
-      key: `${tableName}/2022-03-13.json`,
+      key: `${tableType}/2022-03-13.json`,
       modified: new Date('2022-03-14T12:00:00Z'),
     },
   ])
   s3Client.getObject.mockReturnValue(
-    fs.readFile(path.resolve(__dirname, `./s3Bucket/${tableName}/2022-03-13.json`), { encoding: 'utf8' })
+    fs.readFile(path.resolve(__dirname, `./s3Bucket/${tableType}/2022-03-13.json`), { encoding: 'utf8' })
   )
 }
 
 export function mockSdkS3ClientReponse(
   send: jest.Mock<Promise<GetObjectOutput | ListObjectsV2Output>, [GetObjectCommand | ListObjectsV2Command]>,
-  tableName: string
+  tableType: TableType
 ) {
   const listObjectsResponse: ListObjectsV2Output = {
-    Contents: [{ Key: `${tableName}/2022-03-13.json`, LastModified: new Date('2022-03-14T12:00:00Z') }],
+    Contents: [{ Key: `${tableType}/2022-03-13.json`, LastModified: new Date('2022-03-14T12:00:00Z') }],
   }
   send.mockImplementation(async command => {
     if (command instanceof ListObjectsV2Command) {
       return listObjectsResponse
     }
     if (command instanceof GetObjectCommand) {
-      const stream = createReadStream(path.resolve(__dirname, `../testData/s3Bucket/${tableName}/2022-03-13.json`))
+      const stream = createReadStream(path.resolve(__dirname, `../testData/s3Bucket/${tableType}/2022-03-13.json`))
       const getObjectResponse: GetObjectOutput = { Body: stream }
       return getObjectResponse
     }

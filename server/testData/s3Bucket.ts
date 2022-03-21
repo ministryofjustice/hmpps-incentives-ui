3c436857e69f2a7a01bdfbd3dc5f1036498097a2
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-21 22:11:54 +0000

    INC-289: Allow mocking of empty analytics tables …which mimics filtering by a prison or protected characteristic with no results

--- server/testData/s3Bucket.ts
+++ server/testData/s3Bucket.ts
@@ -8,7 +8,7 @@ import type { GetObjectOutput, ListObjectsV2Output } from '@aws-sdk/client-s3'
 import type S3Client from '../data/s3Client'
 import type { TableType } from '../services/analyticsServiceTypes'
 
-export function mockAppS3ClientResponse(s3Client: jest.Mocked<S3Client>, tableType: TableType) {
+export function mockAppS3ClientResponse(s3Client: jest.Mocked<S3Client>, tableType: TableType, emptyTable = false) {
   s3Client.listObjects.mockResolvedValue([
     {
       key: `${tableType}/2022-03-13.json`,
@@ -16,13 +16,16 @@ export function mockAppS3ClientResponse(s3Client: jest.Mocked<S3Client>, tableTy
     },
   ])
   s3Client.getObject.mockReturnValue(
-    fs.readFile(path.resolve(__dirname, `./s3Bucket/${tableType}/2022-03-13.json`), { encoding: 'utf8' })
+    fs.readFile(path.resolve(__dirname, `./s3Bucket/${tableType}/${emptyTable ? 'empty' : '2022-03-13'}.json`), {
+      encoding: 'utf8',
+    })
   )
 }
 
 export function mockSdkS3ClientReponse(
   send: jest.Mock<Promise<GetObjectOutput | ListObjectsV2Output>, [GetObjectCommand | ListObjectsV2Command]>,
-  tableType: TableType
+  tableType: TableType,
+  emptyTable = false
 ) {
   const listObjectsResponse: ListObjectsV2Output = {
     Contents: [{ Key: `${tableType}/2022-03-13.json`, LastModified: new Date('2022-03-14T12:00:00Z') }],
@@ -32,9 +35,10 @@ export function mockSdkS3ClientReponse(
       return listObjectsResponse
     }
     if (command instanceof GetObjectCommand) {
-      const stream = createReadStream(path.resolve(__dirname, `../testData/s3Bucket/${tableType}/2022-03-13.json`))
-      const getObjectResponse: GetObjectOutput = { Body: stream }
-      return getObjectResponse
+      const stream = createReadStream(
+        path.resolve(__dirname, `../testData/s3Bucket/${tableType}/${emptyTable ? 'empty' : '2022-03-13'}.json`)
+      )
+      return { Body: stream }
     }
     throw new Error('Not implemented')
   })

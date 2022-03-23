adce9e5aff47f60c1643149d0e80f8e10a3a56f3
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-23 22:11:24 +0000

    INC-381: Add a request handler gate that restricts access to users in predefined list

--- server/middleware/featureGate.ts
+++ server/middleware/featureGate.ts
@@ -16,6 +16,7 @@ export function featureGate(flag: string, handler: RequestHandler): RequestHandl
 
 /**
  * Returns true if the user (typically res.locals.user) has an *active* case load which appears in given prisons
+ * If "*" appears in the prisons list, any user passes
  */
 export function userActiveCaseloadMatches(prisons: string[], user?: Express.User): boolean {
   const allowAnyCaseloads = prisons.includes('*')
@@ -33,6 +34,32 @@ export function userActiveCaseloadMatches(prisons: string[], user?: Express.User
 export function activeCaseloadGate(prisons: string[], handler: RequestHandler): RequestHandler {
   return (req: Request, res: Response, next: NextFunction): void => {
     if (userActiveCaseloadMatches(prisons, res.locals.user)) {
+      handler(req, res, next)
+    } else {
+      next(new NotFound())
+    }
+  }
+}
+
+/**
+ * Returns true if the user (typically res.locals.user) username appears in given list
+ * If "*" appears in the username list, any user passes
+ */
+export function usernameMatches(usernames: string[], user?: Express.User): boolean {
+  const allowAnyUsername = usernames.includes('*')
+  if (allowAnyUsername) {
+    return true
+  }
+  return user?.username && usernames.includes(user.username)
+}
+
+/**
+ * Wraps a request handler and returns 404 unless the user’s username appears in given list
+ * If "*" appears in the username list, call is always forwarded to request handler
+ */
+export function usernameGate(usernames: string[], handler: RequestHandler): RequestHandler {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    if (usernameMatches(usernames, res.locals.user)) {
       handler(req, res, next)
     } else {
       next(new NotFound())

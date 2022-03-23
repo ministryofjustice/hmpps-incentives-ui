3296bbe3665d6e71e60ea5b57d9542f2d2017b11
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2022-03-23 14:54:29 +0000

    INC-385: Remove 'Days since last review'/'Days on level' columns
    "Removing" both columns as decided. This is behind a feature flag (
    `FEATURE_HIDE_DAYS_COLUMNS_IN_INCENTIVES_TABLE`).
    
    The columns are hidden for the moment but at some point we could remove
    the feature flag and actually remove the markup in the template.

--- integration_tests/pages/behaviourEntriesPage.ts
+++ integration_tests/pages/behaviourEntriesPage.ts
@@ -11,23 +11,21 @@ export default class BehaviourEntriesPage extends Page {
       .find('.govuk-table__body tr')
       .spread((...rest) =>
         rest.map(element => {
-          const tds = Cypress.$(element).find('td.govuk-table__cell')
+          const tds = Cypress.$(element).find('td.govuk-table__cell:visible')
           return {
             imageSrc: Cypress.$(tds[0]).find('img').attr('src'),
             name: Cypress.$(tds[1]).find('a').html(),
             nameLink: Cypress.$(tds[1]).find('a').attr('href'),
-            daysOnLevel: Cypress.$(tds[2]).text(),
-            daysSinceLastReview: Cypress.$(tds[3]).text(),
-            positiveBehaviours: Cypress.$(tds[4]).text(),
-            positiveBehavioursLink: Cypress.$(tds[4]).find('a').attr('href'),
-            incentiveEncouragements: Cypress.$(tds[5]).text(),
-            incentiveEncouragementsLink: Cypress.$(tds[5]).find('a').attr('href'),
-            negativeBehaviours: Cypress.$(tds[6]).text(),
-            negativeBehavioursLink: Cypress.$(tds[6]).find('a').attr('href'),
-            incentiveWarnings: Cypress.$(tds[7]).text(),
-            incentiveWarningsLink: Cypress.$(tds[7]).find('a').attr('href'),
-            provenAdjudications: Cypress.$(tds[8]).text(),
-            provenAdjudicationsLink: Cypress.$(tds[8]).find('a').attr('href'),
+            positiveBehaviours: Cypress.$(tds[2]).text(),
+            positiveBehavioursLink: Cypress.$(tds[2]).find('a').attr('href'),
+            incentiveEncouragements: Cypress.$(tds[3]).text(),
+            incentiveEncouragementsLink: Cypress.$(tds[3]).find('a').attr('href'),
+            negativeBehaviours: Cypress.$(tds[4]).text(),
+            negativeBehavioursLink: Cypress.$(tds[4]).find('a').attr('href'),
+            incentiveWarnings: Cypress.$(tds[5]).text(),
+            incentiveWarningsLink: Cypress.$(tds[5]).find('a').attr('href'),
+            provenAdjudications: Cypress.$(tds[6]).text(),
+            provenAdjudicationsLink: Cypress.$(tds[6]).find('a').attr('href'),
           }
         })
       )

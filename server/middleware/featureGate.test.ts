8e1fd2415222a544c8576c80b8342c686c5f3abc
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-23 19:42:35 +0000

    INC-388: Refactor feature gate tests to make component reusable

--- server/middleware/featureGate.test.ts
+++ server/middleware/featureGate.test.ts
@@ -1,43 +1,65 @@
 import type { NextFunction, Request, Response, RequestHandler } from 'express'
 import featureGate from './featureGate'
 
+/** Trivial request handler that always returns "OK" */
+const simpleRequestHandler: RequestHandler = (req, res) => {
+  res.send('OK')
+}
+
+/**
+ * Creates a mock request with given app locals
+ */
+function mockRequest(appLocals: Record<string, unknown> = {}): Request {
+  return {
+    app: { locals: appLocals },
+  } as Request
+}
+
+/**
+ * Creates a mock response with given response locals
+ * NB: the send method will track calls
+ */
+function mockResponse(locals: Record<string, unknown> = {}): Response {
+  return {
+    locals,
+    send: jest.fn(),
+  } as unknown as Response
+}
+
+/**
+ * Asserts that the handler successfully responds
+ */
+function expectRequestHandlerToBeCalled(handler: RequestHandler, req: Request, res: Response): void {
+  const next: jest.Mock<NextFunction> = jest.fn()
+  handler(req, res, next)
+  expect(res.send).toBeCalledWith('OK')
+  expect(next).not.toBeCalled()
+}
+
+/**
+ * Asserts that the handler is not called and a 404 is passed to the next handler
+ */
+function expectRequestHandlerTo404(handler: RequestHandler, req: Request, res: Response): void {
+  const next: jest.Mock<NextFunction> = jest.fn()
+  handler(req, res, next)
+  expect(res.send).not.toBeCalled()
+  expect(next).toBeCalled()
+  expect(next.mock.calls[0][0].status).toEqual(404)
+}
+
 describe('featureGate', () => {
-  const gatedHandler: RequestHandler = featureGate('testFlag', (req, res) => {
-    res.send('OK')
-  })
+  /** Gated request handler that requires flag "testFlag" */
+  const gatedHandler = featureGate('testFlag', simpleRequestHandler)
 
   it('calls handler when feature is turned on', () => {
-    const req = {
-      app: {
-        locals: {
-          featureFlags: {
-            testFlag: true,
-          },
-        },
-      },
-    } as unknown as Request
-    const res = { send: jest.fn() } as unknown as Response
-    const next: jest.Mock<NextFunction> = jest.fn()
-    gatedHandler(req, res, next)
-    expect(res.send).toBeCalledWith('OK')
-    expect(next).not.toBeCalled()
+    const req = mockRequest({ featureFlags: { testFlag: true } })
+    const res = mockResponse()
+    expectRequestHandlerToBeCalled(gatedHandler, req, res)
   })
 
   it('returns 404 when feature is turned off', () => {
-    const req = {
-      app: {
-        locals: {
-          featureFlags: {
-            testFlag: false,
-          },
-        },
-      },
-    } as unknown as Request
-    const res = { send: jest.fn() } as unknown as Response
-    const next: jest.Mock<NextFunction> = jest.fn()
-    gatedHandler(req, res, next)
-    expect(res.send).not.toBeCalled()
-    expect(next).toBeCalled()
-    expect(next.mock.calls[0][0].status).toEqual(404)
+    const req = mockRequest({ featureFlags: { testFlag: false } })
+    const res = mockResponse()
+    expectRequestHandlerTo404(gatedHandler, req, res)
   })
 })

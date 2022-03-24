473d1e52e627af1b9324715e1b8dfc53b0e490f4
Darren Oakley <darren.oakley@digital.justice.gov.uk>
2022-03-24 09:08:21 +0000

    Setup prometheus metrics by default. (#75)
    This change sets up prometheus metrics to be available on port 3001, and
    with the helm chart changes they will automatically get scraped and be
    available for alerts and dashboards in grafana.
    
    The added metrics include:
    
    - General nodejs stats: memory use, gc etc
    - HTTP server requests: counters and timings of all served HTTP requests
      by the app.
    - HTTP client requests: counters and timings of all HTTP requests to
      other upstream APIs (as long as they are based off `restClient.ts`).
    - Upstream healthchecks: guages recording the status/health of each
      upstream service when the healthcheck is tested.
    
    This is all backported from the `manage-recalls-ui` app, please let me
    know what you think. :)

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,18 @@
+import promClient from 'prom-client'
+
 import createApp from './app'
 import HmppsAuthClient from './data/hmppsAuthClient'
 import { createRedisClient } from './data/redisClient'
 import TokenStore from './data/tokenStore'
+import { createMetricsApp } from './monitoring/metricsApp'
 import UserService from './services/userService'
+
+promClient.collectDefaultMetrics()
 
 const hmppsAuthClient = new HmppsAuthClient(new TokenStore(createRedisClient('server/index.ts')))
 const userService = new UserService(hmppsAuthClient)
 
 const app = createApp(userService)
+const metricsApp = createMetricsApp()
 
-export default app
+export { app, metricsApp }

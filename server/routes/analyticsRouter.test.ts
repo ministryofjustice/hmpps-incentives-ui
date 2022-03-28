c5f5a75b7ba7093f24ee1c7bc9151c613574ef53
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-03-28 16:17:07 +0100

    INC-412: Post chart feedback forms to Zendesk … by intercepting form data submitted to any chart page:
    • chart page routes now allow GET and POST
    • when there is a POST, the form data submitted is validated
    • if it is valid, a ticket creation request is sent to Zendesk
    • if it is not valid, the invalid form is stored in the response locals
    • in either case, the request is passed to the next (GET) handler which displays charts
    
    NB: no HTML forms exist on the chart pages so actual form submissions are not possible

--- server/routes/analyticsRouter.test.ts
+++ server/routes/analyticsRouter.test.ts
@@ -2,11 +2,13 @@ import type { Express } from 'express'
 import request from 'supertest'
 
 import config from '../config'
+import ZendeskClient from '../data/zendeskClient'
 import { TableType } from '../services/analyticsServiceTypes'
 import { appWithAllRoutes } from './testutils/appSetup'
 import { MockTable, mockSdkS3ClientReponse } from '../testData/s3Bucket'
 
 jest.mock('@aws-sdk/client-s3')
+jest.mock('../data/zendeskClient')
 
 const s3 = {
   send: jest.fn(),
@@ -85,6 +87,7 @@ const analyticsPages = [
     linksToIncentivesTable: true,
     sampleLocations: ['1', '2', '3', '4', '5', '6', '7', '8', 'SEG'],
     sourceTable: TableType.behaviourEntries,
+    graphIds: ['entries-by-location', 'prisoners-with-entries-by-location'],
   },
   {
     name: 'Incentive levels',
@@ -93,6 +96,7 @@ const analyticsPages = [
     linksToIncentivesTable: true,
     sampleLocations: ['1', '2', '3', '4', '5', '6', '7', '8', 'SEG'],
     sourceTable: TableType.incentiveLevels,
+    graphIds: ['incentive-levels-by-location'],
   },
   {
     name: 'Protected characteristics',
@@ -100,6 +104,7 @@ const analyticsPages = [
     expectedHeading: 'Percentage and number of prisoners on each incentive level by ethnicity',
     linksToIncentivesTable: false,
     sourceTable: TableType.incentiveLevels,
+    graphIds: ['incentive-levels-by-ethnicity', 'incentive-levels-by-age-group'],
   },
 ]
 
@@ -107,7 +112,7 @@ const samplePrison = 'MDI'
 
 describe.each(analyticsPages)(
   'Analytics data pages',
-  ({ name, url, expectedHeading, linksToIncentivesTable, sampleLocations, sourceTable }) => {
+  ({ name, url, expectedHeading, graphIds, linksToIncentivesTable, sampleLocations, sourceTable }) => {
     beforeEach(() => {
       mockSdkS3ClientReponse(s3.send, sourceTable)
     })
@@ -166,5 +171,103 @@ describe.each(analyticsPages)(
           })
       })
     }
+
+    describe.each(graphIds)('charts have feedback forms', graphId => {
+      let originalZendeskConfig: typeof config.apis.zendesk
+
+      let mockedZendeskClientClass: jest.Mock<ZendeskClient>
+
+      beforeAll(() => {
+        originalZendeskConfig = config.apis.zendesk
+        config.apis.zendesk.url = 'http://zendesk.local'
+        config.apis.zendesk.username = 'anonymous@justice.gov.uk'
+        config.apis.zendesk.token = '123456789012345678901234567890'
+
+        mockedZendeskClientClass = ZendeskClient as jest.Mock<ZendeskClient>
+
+        mockSdkS3ClientReponse(s3.send, sourceTable)
+      })
+
+      afterAll(() => {
+        config.apis.zendesk = originalZendeskConfig
+      })
+
+      it(`${name} page can post simple feedback on ${graphId} chart`, () => {
+        return request(app)
+          .post(url)
+          .send({ formId: graphId, chartUseful: 'yes' })
+          .expect(200)
+          .expect(res => {
+            expect(res.text).toContain(expectedHeading)
+            expect(mockedZendeskClientClass).toHaveBeenCalled()
+            const mockedZendeskClient = mockedZendeskClientClass.mock.instances[0] as jest.Mocked<ZendeskClient>
+            expect(mockedZendeskClient.createTicket).toHaveBeenCalledWith({
+              subject: `Feedback on chart ${graphId}`,
+              comment: { body: expect.any(String) },
+              type: 'task',
+              tags: ['hmpps-incentives', 'chart-feedback', 'useful-yes'],
+              custom_fields: [
+                // Service
+                { id: 23757677, value: 'hmpps_incentives' },
+                // Environment
+                { id: 32342378, value: config.environment },
+                // URL
+                { id: 23730083, value: expect.stringContaining(url) },
+                // Prison
+                { id: 23984153, value: 'MDI' },
+              ],
+            })
+            const createTicketRequest = mockedZendeskClient.createTicket.mock.calls[0][0]
+            expect(createTicketRequest.comment.body).toContain('Is this chart useful? yes')
+            expect(createTicketRequest.comment.body).toContain('Prison: MDI')
+            expect(createTicketRequest.comment.body).not.toContain('Comments:')
+          })
+      })
+
+      it(`${name} page can post more complex feedback on ${graphId} chart`, () => {
+        return request(app)
+          .post(url)
+          .send({
+            formId: graphId,
+            chartUseful: 'no',
+            mainNoReason: 'do-not-understand',
+            noComments: 'How do I use this?',
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.text).toContain(expectedHeading)
+            const mockedZendeskClient = mockedZendeskClientClass.mock.instances[0] as jest.Mocked<ZendeskClient>
+            expect(mockedZendeskClient.createTicket).toHaveBeenCalledWith({
+              subject: expect.any(String),
+              comment: { body: expect.any(String) },
+              type: 'task',
+              tags: ['hmpps-incentives', 'chart-feedback', 'useful-no', 'not-useful-do-not-understand'],
+              custom_fields: expect.anything(),
+            })
+            expect(mockedZendeskClientClass).toHaveBeenCalled()
+            const createTicketRequest = mockedZendeskClient.createTicket.mock.calls[0][0]
+            expect(createTicketRequest.comment.body).toContain('Is this chart useful? no')
+            expect(createTicketRequest.comment.body).toContain('Prison: MDI')
+            expect(createTicketRequest.comment.body).toContain('Main reason: do-not-understand')
+            expect(createTicketRequest.comment.body).toContain('Comments:')
+            expect(createTicketRequest.comment.body).toContain('How do I use this?')
+          })
+      })
+
+      it(`${name} page will not post invalid feedback on ${graphId} chart`, () => {
+        return request(app)
+          .post(url)
+          .send({
+            formId: graphId,
+            chartUseful: 'no',
+            noComments: 'Do I have to choose only one reason?',
+          })
+          .expect(200)
+          .expect(res => {
+            expect(res.text).toContain(expectedHeading)
+            expect(mockedZendeskClientClass).not.toHaveBeenCalled()
+          })
+      })
+    })
   }
 )
--- server/routes/analyticsRouter.ts
+++ server/routes/analyticsRouter.ts
@@ -1,11 +1,16 @@
-import type { RequestHandler, Router } from 'express'
+import type { NextFunction, Request, RequestHandler, Response, Router } from 'express'
+import { MethodNotAllowed } from 'http-errors'
 
 import config from '../config'
+import logger from '../../logger'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 import { featureGate, activeCaseloadGate, usernameGate } from '../middleware/featureGate'
 import S3Client from '../data/s3Client'
+import ZendeskClient, { CreateTicketRequest } from '../data/zendeskClient'
 import AnalyticsService from '../services/analyticsService'
 import { ProtectedCharacteristic } from '../services/analyticsServiceTypes'
+import Form from './forms/forms'
+import { type ChartFeedbackForm, validate } from './forms/chartFeedbackForm'
 
 /**
  * Shared template variables needed throughout analytics section
@@ -23,24 +28,40 @@ function urlForLocation(prison: string, location: string): string {
   return `/incentive-summary/${prison}-${location}`
 }
 
-export default function routes(router: Router): Router {
-  const get = (path: string, handler: RequestHandler) =>
-    router.get(
-      path,
-      featureGate(
-        'showAnalytics',
-        activeCaseloadGate(
-          config.prisonsWithAnalytics,
-          usernameGate(config.usernamesWithAnalytics, asyncMiddleware(handler))
-        )
-      )
+/**
+ * Adds global, per-prison and per-user feature gates to an analytics request handler
+ */
+function addFeatureGates(asyncHandler: RequestHandler): RequestHandler {
+  return featureGate(
+    'showAnalytics',
+    activeCaseloadGate(
+      config.prisonsWithAnalytics,
+      usernameGate(config.usernamesWithAnalytics, asyncMiddleware(asyncHandler))
     )
+  )
+}
+
+export default function routes(router: Router): Router {
+  const get = (path: string, handler: RequestHandler) => router.get(path, addFeatureGates(handler))
 
   get('/', (req, res) => {
     res.redirect('/analytics/incentive-levels')
   })
 
-  get('/behaviour-entries', async (req, res) => {
+  const routeWithFeedback = (path: string, handler: RequestHandler) =>
+    router.all(
+      path,
+      addFeatureGates((req, res, next) => {
+        if (req.method !== 'GET' && req.method !== 'POST') {
+          next(new MethodNotAllowed())
+        }
+        next()
+      }),
+      addFeatureGates(chartFeedbackHandler),
+      addFeatureGates(handler)
+    )
+
+  routeWithFeedback('/behaviour-entries', async (req, res) => {
     res.locals.breadcrumbs.addItems({ text: 'Behaviour entries' })
 
     const activeCaseLoad = res.locals.user.activeCaseload.id
@@ -57,7 +78,7 @@ export default function routes(router: Router): Router {
     })
   })
 
-  get('/incentive-levels', async (req, res) => {
+  routeWithFeedback('/incentive-levels', async (req, res) => {
     res.locals.breadcrumbs.addItems({ text: 'Incentive levels' })
 
     const activeCaseLoad = res.locals.user.activeCaseload.id
@@ -72,7 +93,7 @@ export default function routes(router: Router): Router {
     })
   })
 
-  get('/protected-characteristics', async (req, res) => {
+  routeWithFeedback('/protected-characteristics', async (req, res) => {
     res.locals.breadcrumbs.addItems({ text: 'Protected characteristics' })
 
     const activeCaseLoad = res.locals.user.activeCaseload.id
@@ -96,4 +117,92 @@ export default function routes(router: Router): Router {
   })
 
   return router
+}
+
+async function chartFeedbackHandler(req: Request, res: Response, next: NextFunction) {
+  if (req.method !== 'POST') {
+    next()
+    return
+  }
+
+  const activeCaseLoad = res.locals.user.activeCaseload.id
+  const url = `${req.protocol}://${req.get('host')}${req.originalUrl}`
+  const data = req.body as Partial<ChartFeedbackForm>
+
+  if (!data.formId) {
+    logger.error('Form posted without specifying formId')
+    next()
+    return
+  }
+
+  const form = new Form<ChartFeedbackForm>(data)
+  validate(form)
+  if (form.hasErrors) {
+    // form has errors so should be re-displayed
+    res.locals.formsWithErrors = { [form.data.formId]: form }
+    next()
+    return
+  }
+
+  logger.info(`Submitting feedback to Zendesk: Chart ${form.data.formId} was useful=${form.data.chartUseful}`)
+
+  const tags = ['hmpps-incentives', 'chart-feedback', `useful-${form.data.chartUseful}`]
+  if (form.data.chartUseful === 'no') {
+    tags.push(`not-useful-${form.data.mainNoReason}`)
+  }
+
+  let comment =
+    form.data.chartUseful === 'yes'
+      ? `
+Chart: ${form.data.formId} (${url})
+Prison: ${activeCaseLoad}
+
+Is this chart useful? ${form.data.chartUseful}`
+      : `
+Chart: ${form.data.formId} (${url})
+Prison: ${activeCaseLoad}
+
+Is this chart useful? ${form.data.chartUseful}
+Main reason: ${form.data.mainNoReason}`
+  if (form.data.yesComments) {
+    comment += `
+
+Comments:
+${form.data.yesComments}`
+  }
+  if (form.data.noComments) {
+    comment += `
+
+Comments:
+${form.data.noComments}`
+  }
+
+  const ticket: CreateTicketRequest = {
+    subject: `Feedback on chart ${form.data.formId}`,
+    comment: { body: comment.trim() },
+    type: 'task',
+    tags,
+    custom_fields: [
+      // Service
+      { id: 23757677, value: 'hmpps_incentives' },
+      // Environment
+      { id: 32342378, value: config.environment },
+      // URL
+      { id: 23730083, value: url },
+      // Prison
+      { id: 23984153, value: activeCaseLoad },
+    ],
+  }
+  const { username, token } = config.apis.zendesk
+  if (username && token) {
+    const zendesk = new ZendeskClient(config.apis.zendesk, username, token)
+    try {
+      await zendesk.createTicket(ticket)
+    } catch (error) {
+      logger.error('Failed to create Zendesk ticket', error)
+    }
+  } else {
+    logger.error('No Zendesk credetials. Cannot create ticket.')
+  }
+  next()
 }

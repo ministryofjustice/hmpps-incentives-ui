aaae6fe82ae74b2cbf076a137536a7abba9264e7
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2022-05-03 10:56:52 +0100

    Adding better redis error handling (hmpps-template-typescript#84)

--- server/data/redisClient.ts
+++ server/data/redisClient.ts
@@ -14,11 +14,17 @@ export const createRedisClient = (clientName: string, legacyMode = false): Redis
     url,
     password: config.redis.password,
     legacyMode,
+    socket: {
+      reconnectStrategy: (attempts: number) => {
+        // Exponential back off: 20ms, 40ms, 80ms..., capped to retry every 30 seconds
+        const nextDelay = Math.min(2 ** attempts * 20, 30000)
+        logger.info(`Retry [${clientName}] Redis connection attempt: ${attempts}, next attempt in: ${nextDelay}ms`)
+        return nextDelay
+      },
+    },
   })
 
-  client.on('error', error => {
-    logger.error(`[${clientName}] Redis error`, error)
-  })
+  client.on('error', (e: Error) => logger.error(`[${clientName}] Redis error`, e))
 
   return client
 }

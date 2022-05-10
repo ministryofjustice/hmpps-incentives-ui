25b60980ac909186b9b2959e43faad50c9fdf9b4
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-05-10 15:44:20 +0100

    Ensure that all Google Analytics globals are defined in integration tests

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -12,7 +12,10 @@ declare namespace Cypress {
    * Declare globals
    */
   interface ApplicationWindow {
-    // Google Analytics function
+    /** Google Analytics – Universal Analytics (deprecated) */
     ga?: (...args: string[]) => void
+
+    /** Google Analytics version 4 */
+    gtag?: (...args: [string, string, Record<string, string>]) => void
   }
 }

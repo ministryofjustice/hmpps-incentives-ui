122eda7738bc649df75e07c98ecba8c025530485
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2022-05-16 13:53:43 +0100

    Allow async get to take an array of strings for paths like original get method (hmpps-template-typescript#90)

--- server/routes/home.ts
+++ server/routes/home.ts
@@ -3,7 +3,7 @@ import type { RequestHandler, Router } from 'express'
 import asyncMiddleware from '../middleware/asyncMiddleware'
 
 export default function routes(router: Router): Router {
-  const get = (path: string, handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
 
   get('/', async (req, res, next) => {
     res.locals.breadcrumbs.lastItem.href = undefined

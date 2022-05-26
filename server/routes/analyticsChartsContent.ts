4867aac133b7004c3f614add398afc40fff75498
Neil Mendum <neil.mendum@digital.justice.gov.uk>
2022-05-26 10:59:53 +0100

    INC-585: update PC charts to use generic content for guidance text (#201)

--- server/routes/analyticsChartsContent.ts
+++ server/routes/analyticsChartsContent.ts
@@ -46,10 +46,14 @@ export const IncentiveLevelsChartsContent: Record<string, AnalyticsChartContent>
   },
 }
 
+const guidanceIncentiveLevelsByPc =
+  'Use this chart to see incentive levels for this protected characteristic and compare them to prison levels. Are there patterns or imbalances that you might want to explore further? Be aware of small numbers.'
 const guidanceTrendsIncentiveLevelsByPc =
-  'Use this chart to compare incentive levels with previous months. Information is taken from monthly averages. Is there anything that needs explored further? Is the incentives scheme producing the levels you would like it to?'
+  'Use this chart to compare incentive levels for each group in this protected characteristic with previous months. Information is taken from monthly averages. Is there anything that needs explored further? Is the incentives scheme producing levels you expect?'
 const guidanceTrendsEntriesByPc =
   'Use this chart to compare the numbers of positive, negative and total entries received by each group within this protected characteristic with previous months. What do the number of entries and the ratios tell you about incentives policy and behaviour of prisoners? Be aware of population changes.'
+const guidanceEntriesByPc =
+  'Use this chart to see the number of prisoners who have received different types of behaviour entries in this protected characteristic. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.'
 export const ProtectedCharacteristicsChartsContent: Record<string, AnalyticsChartContent> = {
   'population-by-age': {
     title: 'Percentage and number of prisoners in the establishment by age',
@@ -80,38 +84,33 @@ export const ProtectedCharacteristicsChartsContent: Record<string, AnalyticsChar
   },
   'incentive-levels-by-age': {
     title: 'Percentage and number of prisoners on each incentive level by age',
-    guidance:
-      'Use this chart to see incentive levels for the main age groups and compare them to prison levels. Are there patterns that suggest the incentives policy isn’t working as well for some groups?',
+    guidance: guidanceIncentiveLevelsByPc,
     labelColumn: 'Age',
     googleAnalyticsCategory: 'Incentive level by age',
   },
   'incentive-levels-by-disability': {
     title: 'Percentage and number of prisoners on each incentive level by recorded disability',
-    guidance:
-      'This chart will help you tell if there is any imbalance in incentive levels between prisoners with a disability recorded on NOMIS and those without. As the amount of missing data is high, the number of prisoners with unknown information is also provided.',
+    guidance: guidanceIncentiveLevelsByPc,
     labelColumn: 'Disability',
     googleAnalyticsCategory: 'Incentive level by disability',
     wideLabel: true,
   },
   'incentive-levels-by-ethnicity': {
     title: 'Percentage and number of prisoners on each incentive level by ethnicity',
-    guidance:
-      'Use this chart to see incentive levels for the main ethnicity groups and compare them to prison levels. Are there patterns or imbalances that you might want to explore further?',
+    guidance: guidanceIncentiveLevelsByPc,
     labelColumn: 'Ethnicity',
     googleAnalyticsCategory: 'Incentive level by ethnicity',
     wideLabel: true,
   },
   'incentive-levels-by-religion': {
     title: 'Percentage and number of prisoners on each incentive level by religion',
-    guidance:
-      'Use this chart to see how incentive levels might vary for prisoners of different religions. Comparing to the overall prison levels should help you to spot any imbalances, but be aware of generalising from small numbers.',
+    guidance: guidanceIncentiveLevelsByPc,
     labelColumn: 'Religion',
     googleAnalyticsCategory: 'Incentive level by religion',
   },
   'incentive-levels-by-sexual-orientation': {
     title: 'Percentage and number of prisoners on each incentive level by sexual orientation',
-    guidance:
-      'Use this chart to see incentive levels for prisoners of different sexual orientation and compare them to the overall prison levels. Are there any patterns or imbalances that you might want to explore further?',
+    guidance: guidanceIncentiveLevelsByPc,
     labelColumn: 'Sexual orientation',
     googleAnalyticsCategory: 'Incentive level by sexual orientation',
   },
@@ -167,40 +166,35 @@ export const ProtectedCharacteristicsChartsContent: Record<string, AnalyticsChar
   },
   'entries-by-age': {
     title: 'Percentage and number of prisoners receiving each behaviour entry type by age – last 28 days',
-    guidance:
-      'Use this chart to see the number of prisoners with different types of behaviour entries for each age grouping. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.',
+    guidance: guidanceEntriesByPc,
     labelColumn: 'Age',
     googleAnalyticsCategory: 'Behaviour entries by age',
   },
   'entries-by-disability': {
     title:
       'Percentage and number of prisoners receiving each behaviour entry type by recorded disability – last 28 days',
-    guidance:
-      'Use this chart to see the number of prisoners with different types of behaviour entries for each disability grouping. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.',
+    guidance: guidanceEntriesByPc,
     labelColumn: 'Disability',
     googleAnalyticsCategory: 'Behaviour entries by disability',
     wideLabel: true,
   },
   'entries-by-ethnicity': {
     title: 'Percentage and number of prisoners receiving each behaviour entry type by ethnicity – last 28 days',
-    guidance:
-      'Use this chart to see the number of prisoners with different types of behaviour entries for each ethnic grouping. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.',
+    guidance: guidanceEntriesByPc,
     labelColumn: 'Ethnicity',
     googleAnalyticsCategory: 'Behaviour entries by ethnicity',
     wideLabel: true,
   },
   'entries-by-religion': {
     title: 'Percentage and number of prisoners receiving each behaviour entry type by religion – last 28 days',
-    guidance:
-      'Use this chart to see the number of prisoners with different types of behaviour entries for each religious grouping. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.',
+    guidance: guidanceEntriesByPc,
     labelColumn: 'Religion',
     googleAnalyticsCategory: 'Behaviour entries by religion',
   },
   'entries-by-sexual-orientation': {
     title:
       'Percentage and number of prisoners receiving each behaviour entry type by sexual orientation – last 28 days',
-    guidance:
-      'Use this chart to see the number of prisoners with different types of behaviour entries for each sexual orientation grouping. Are there imbalances that need action to make sure incentives work for everyone? Be cautious about small numbers.',
+    guidance: guidanceEntriesByPc,
     labelColumn: 'Sexual orientation',
     googleAnalyticsCategory: 'Behaviour entries by sexual orientation',
   },

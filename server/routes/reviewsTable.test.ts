43ac6c9bed5762d3b7ebb6bfa0ae7f1ad08cec22
ushkarev <ushkarev@users.noreply.github.com>
2022-10-12 16:33:03 +0100

    INC-751 & INC-747: Placeholder reviews table with "dashboard" and tabs (#307)
    * Add placeholder "dashboard" to reviews table page showing number of overdue reviews
    
    * Add tabs for each incentive level available in selected prison
    
    * Allow navigating between incentive levels in reviews table

--- server/routes/reviewsTable.test.ts
+++ server/routes/reviewsTable.test.ts
@@ -1,11 +1,15 @@
 import type { Express } from 'express'
+import jquery from 'jquery'
+import { JSDOM } from 'jsdom'
 import request from 'supertest'
 
 import config from '../config'
 import { appWithAllRoutes } from './testutils/appSetup'
 import HmppsAuthClient from '../data/hmppsAuthClient'
+import { IncentivesApi, type Level } from '../data/incentivesApi'
 
 jest.mock('../data/hmppsAuthClient')
+jest.mock('../data/incentivesApi')
 
 let app: Express
 
@@ -15,7 +19,31 @@ beforeEach(() => {
 
   const hmppsAuthClient = HmppsAuthClient.prototype as jest.Mocked<HmppsAuthClient>
   hmppsAuthClient.getSystemClientToken.mockResolvedValue('test system token')
+
+  const incentivesApi = IncentivesApi.prototype as jest.Mocked<IncentivesApi>
+  incentivesApi.getAvailableLevels.mockResolvedValue(sampleLevels)
 })
+
+const sampleLevels: Level[] = [
+  {
+    iepLevel: 'BAS',
+    iepDescription: 'Basic',
+    sequence: 1,
+    default: false,
+  },
+  {
+    iepLevel: 'STD',
+    iepDescription: 'Standard',
+    sequence: 2,
+    default: true,
+  },
+  {
+    iepLevel: 'ENH',
+    iepDescription: 'Enhanced',
+    sequence: 3,
+    default: false,
+  },
+]
 
 describe('Reviews table', () => {
   it('should show selected location', () => {
@@ -25,6 +53,47 @@ describe('Reviews table', () => {
       .expect(res => {
         expect(res.text).toContain('Manage incentive reviews')
         expect(res.text).toContain('A wing')
+      })
+  })
+
+  it('should show number of overdue reviews', () => {
+    return request(app)
+      .get('/incentive-summary/MDI-2')
+      .expect('Content-Type', /html/)
+      .expect(res => {
+        expect(res.text).toContain('Manage incentive reviews')
+        expect(res.text).toContain('<dd>16</dd>')
+      })
+  })
+
+  it('shows tabs for each available level', () => {
+    const incentivesApi = IncentivesApi.prototype as jest.Mocked<IncentivesApi>
+    incentivesApi.getAvailableLevels.mockResolvedValue(sampleLevels)
+
+    return request(app)
+      .get('/incentive-summary/MDI-1')
+      .expect(res => {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const { iepLevel } of sampleLevels) {
+          expect(res.text).toContain(`?level=${iepLevel}`)
+        }
+      })
+  })
+
+  it.each([
+    ['', 'Standard'],
+    ['?level=BAS', 'Basic'],
+    ['?level=ENH', 'Enhanced'],
+    ['?level=EN2', 'Standard'],
+  ])('shows tabs for each available level', (urlSuffix, expectedLevel) => {
+    const $ = jquery(new JSDOM().window) as unknown as typeof jquery
+
+    return request(app)
+      .get(`/incentive-summary/MDI-1${urlSuffix}`)
+      .expect(res => {
+        const $body = $(res.text)
+        const selectedLevel = $body.find('.govuk-tabs__list-item--selected').text().trim()
+        expect(selectedLevel).toEqual(expectedLevel)
       })
   })
 })
--- server/routes/reviewsTable.ts
+++ server/routes/reviewsTable.ts
@@ -2,7 +2,12 @@ import type { RequestHandler, Router } from 'express'
 
 import config from '../config'
 import asyncMiddleware from '../middleware/asyncMiddleware'
+import HmppsAuthClient from '../data/hmppsAuthClient'
+import { createRedisClient } from '../data/redisClient'
+import { IncentivesApi } from '../data/incentivesApi'
+import TokenStore from '../data/tokenStore'
 
+const hmppsAuthClient = new HmppsAuthClient(new TokenStore(createRedisClient('routes/incentivesTable.ts')))
 const feedbackUrl = config.feedbackUrlForTable || config.feedbackUrl
 
 export default function routes(router: Router): Router {
@@ -11,9 +16,24 @@ export default function routes(router: Router): Router {
   get('/', async (req, res) => {
     res.locals.breadcrumbs.addItems({ text: 'Manage incentive reviews' })
 
-    const locationDescription = 'A wing'
+    const { user } = res.locals
+    const { locationPrefix } = req.params
+    let { level: selectedLevelCode }: { level?: string } = req.query
+    const agencyId = locationPrefix.split('-')[0]
 
-    res.render('pages/reviewsTable', { feedbackUrl, locationDescription })
+    const systemToken = await hmppsAuthClient.getSystemClientToken(user.username)
+    const incentivesApi = new IncentivesApi(systemToken)
+
+    const levels = await incentivesApi.getAvailableLevels(agencyId)
+    const defaultLevel = levels.find(level => level.default) ?? levels[0]
+    if (!levels.some(level => level.iepLevel === selectedLevelCode)) {
+      selectedLevelCode = defaultLevel.iepLevel
+    }
+
+    const locationDescription = 'A wing'
+    const overdueCount = 16
+
+    res.render('pages/reviewsTable', { feedbackUrl, locationDescription, overdueCount, levels, selectedLevelCode })
   })
 
   return router

71fc7b850f8b49c14aec0788304fb00702afd1f7
ushkarev <ushkarev@users.noreply.github.com>
2022-11-30 16:56:41 +0000

    INC-960: Allow cards on the home page to be clickable anywhere (#339)
    …like those on DPS home page

--- assets/js/application.js
+++ assets/js/application.js
@@ -25,4 +25,18 @@ $(() => {
       })
     }
   })
+
+  // for clickable cards, forward a click anywhere inside it to single contained link, if it exists
+  $('.card--clickable').each((index, card) => {
+    const $links = $('.card__link', card)
+    if ($links.length === 1) {
+      const $card = $(card)
+      $card.on('click', e => {
+        if (e.target.nodeName !== 'A') {
+          e.stopPropagation()
+          $links[0].click()
+        }
+      })
+    }
+  })
 })

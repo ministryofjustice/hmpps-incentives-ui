55ad9472e0793951a6c8af8e8fe024eacfacd3f7
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-04-09 14:21:58 +0100

    Test UI to remove an existing level from a prison

--- integration_tests/e2e/prisonIncentiveLevels.cy.ts
+++ integration_tests/e2e/prisonIncentiveLevels.cy.ts
@@ -5,6 +5,7 @@ import { sampleIncentiveLevels, samplePrisonIncentiveLevels } from '../../server
 import HomePage from '../pages/home'
 import PrisonIncentiveLevelPage from '../pages/prisonIncentiveLevels/prisonIncentiveLevel'
 import PrisonIncentiveLevelAddFormPage from '../pages/prisonIncentiveLevels/prisonIncentiveLevelAddForm'
+import PrisonIncentiveLevelDeactivateFormPage from '../pages/prisonIncentiveLevels/prisonIncentiveLevelDeactivateForm'
 import PrisonIncentiveLevelsPage from '../pages/prisonIncentiveLevels/prisonIncentiveLevels'
 
 context('Prison incentive level management', () => {
@@ -19,11 +20,12 @@ context('Prison incentive level management', () => {
     cy.task('stubPrisonIncentiveLevel')
 
     cy.signIn()
-    const homePage = Page.verifyOnPage(HomePage)
-    homePage.managePrisonIncentiveLevelsLink().click()
   })
 
   it('shows list of levels and details of a level', () => {
+    const homePage = Page.verifyOnPage(HomePage)
+    homePage.managePrisonIncentiveLevelsLink().click()
+
     const listPage = Page.verifyOnPage(PrisonIncentiveLevelsPage)
     listPage.checkLastBreadcrumb()
     listPage.contentsOfTable.should('have.all.key', 'Basic', 'Standard', 'Enhanced')
@@ -59,6 +61,9 @@ context('Prison incentive level management', () => {
     cy.task('stubPrisonIncentiveLevel', { prisonIncentiveLevel: enhanced3 })
     cy.task('stubIncentiveLevel', { incentiveLevel: sampleIncentiveLevels[4] })
 
+    const homePage = Page.verifyOnPage(HomePage)
+    homePage.managePrisonIncentiveLevelsLink().click()
+
     const listPage = Page.verifyOnPage(PrisonIncentiveLevelsPage)
     listPage.addLink.click()
 
@@ -87,5 +92,50 @@ context('Prison incentive level management', () => {
     detailPage.messages.should('contain.text', 'Enhanced 3')
     detailPage.messages.should('contain.text', 'added')
     detailPage.contentsOfTables.its(0).its('Default for new prisoners').should('eq', 'No')
+  })
+
+  it('should allow removing a non-required level', () => {
+    const enhanced2: PrisonIncentiveLevel = {
+      ...samplePrisonIncentiveLevels[2],
+      levelCode: 'EN2',
+      levelName: 'Enhanced 2',
+    }
+    const prisonIncentiveLevels: PrisonIncentiveLevel[] = [
+      ...samplePrisonIncentiveLevels.filter(prisonIncentiveLevel => prisonIncentiveLevel.active),
+      enhanced2,
+    ]
+    cy.task('stubPrisonIncentiveLevels', { prisonId: 'MDI', prisonIncentiveLevels })
+    cy.task('stubPatchPrisonIncentiveLevel', { prisonIncentiveLevel: { ...enhanced2, active: false } })
+    cy.task('stubPrisonIncentiveLevel', { prisonIncentiveLevel: enhanced2 })
+    cy.task('stubIncentiveLevel', { incentiveLevel: sampleIncentiveLevels[3] })
+
+    const homePage = Page.verifyOnPage(HomePage)
+    homePage.managePrisonIncentiveLevelsLink().click()
+    let listPage = Page.verifyOnPage(PrisonIncentiveLevelsPage)
+    listPage.contentsOfTable.should('have.all.key', 'Basic', 'Standard', 'Enhanced', 'Enhanced 2')
+
+    listPage.findTableLink(3, 'Remove level').click()
+
+    let deactivatePage = Page.verifyOnPage(PrisonIncentiveLevelDeactivateFormPage, 'Enhanced 2')
+    deactivatePage.checkLastBreadcrumb()
+
+    deactivatePage.form.submit() // empty form
+    Page.verifyOnPage(PrisonIncentiveLevelDeactivateFormPage, 'Enhanced 2')
+    deactivatePage.errorSummaryTitle.should('contain.text', 'There is a problem')
+
+    deactivatePage.confirmationRadios.eq(1).find('label').click()
+    deactivatePage.form.submit() // form should now be valid
+
+    listPage = Page.verifyOnPage(PrisonIncentiveLevelsPage)
+    listPage.messages.should('not.exist')
+
+    listPage.findTableLink(3, 'Remove level').click()
+
+    deactivatePage = Page.verifyOnPage(PrisonIncentiveLevelDeactivateFormPage, 'Enhanced 2')
+    deactivatePage.confirmationRadios.eq(0).find('label').click()
+    deactivatePage.form.submit() // form should now be valid
+
+    listPage = Page.verifyOnPage(PrisonIncentiveLevelsPage)
+    listPage.messages.should('contain.text', 'Enhanced 2').should('contain.text', 'no longer available')
   })
 })

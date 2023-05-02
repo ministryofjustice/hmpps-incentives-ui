e65f6c580801cd260d7a70808c26422a9eaf31c3
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2023-05-02 14:25:40 +0100

    INC-1252: Update to the form validation in incentiveLevelCreateForm, trimming any leftover spaces in the 'name' field.

--- server/routes/forms/incentiveLevelCreateForm.ts
+++ server/routes/forms/incentiveLevelCreateForm.ts
@@ -9,10 +9,11 @@ export default class IncentiveLevelCreateForm extends Form<IncentiveLevelCreateD
   protected validate(): void {
     if (!this.data.name || this.data.name.length < 1) {
       this.addError('name', 'The level’s name is required')
+    } else {
+      this.data.name.trim()
     }
     // TODO: add max length limit to `name` & error message once determined
     //       NOMIS has hard limit of 40, incentives DB is 30
-
     const codeRE = /^[A-Za-z0-9]{3}$/
     if (!codeRE.test(this.data.code)) {
       this.addError('code', 'The level’s code must be 3 letters or numbers')

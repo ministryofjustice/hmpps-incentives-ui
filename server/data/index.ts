63d3ac723a749d9dafaac4247ce266840cf103de
Ashley Gyngell <ashley.gyngell@digital.justice.gov.uk>
2023-09-22 10:41:44 +0100

    INC-1317: Updating data index file

--- server/data/index.ts
+++ server/data/index.ts
@@ -3,7 +3,7 @@
  * Do appinsights first as it does some magic instrumentation work, i.e. it affects other 'require's
  * In particular, applicationinsights automatically collects bunyan logs
  */
-import { initialiseAppInsights, buildAppInsightsClient } from '../utils/azureAppInsights'
+import { initialiseAppInsights } from '../utils/azureAppInsights'
 import applicationInfo from '../config'
 
 initialiseAppInsights()

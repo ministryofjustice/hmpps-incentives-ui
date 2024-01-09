89a0a6eb09c5a7b5d2fdd106b9a7e7bf3cc0d53c
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-01-09 17:15:03 +0000

    Remove non-existent paths from nunjucks views list

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -41,9 +41,7 @@ export default function nunjucksSetup(app: express.Express): void {
     [
       path.join(__dirname, '../../server/views'),
       'node_modules/govuk-frontend/dist/',
-      'node_modules/govuk-frontend/dist/components/',
       'node_modules/@ministryofjustice/frontend/',
-      'node_modules/@ministryofjustice/frontend/moj/components/',
     ],
     {
       autoescape: true,

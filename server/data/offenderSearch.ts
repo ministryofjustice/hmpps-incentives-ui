cb8c7b6d814acd641d9d9feca9c1c83d5e9dc9fc
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-01-29 14:57:18 +0000

    Remove unused code

--- server/data/offenderSearch.ts
+++ server/data/offenderSearch.ts
@@ -1,5 +1,5 @@
 import config from '../config'
-import { TransferPrisonId, OutsidePrisonId, transferPrisonId, outsidePrisonId } from './constants'
+import type { TransferPrisonId, OutsidePrisonId } from './constants'
 import RestClient from './restClient'
 
 interface BaseOffenderSearchResult {
@@ -29,42 +29,7 @@ export interface OffenderSearchResultOut extends BaseOffenderSearchResult {
 
 export type OffenderSearchResult = OffenderSearchResultIn | OffenderSearchResultTransfer | OffenderSearchResultOut
 
-export function isBeingTransferred(prisoner: OffenderSearchResult): prisoner is OffenderSearchResultTransfer
-export function isBeingTransferred(prisonerOrNonAssociation: { prisonId: string }): boolean
-export function isBeingTransferred(prisonerOrNonAssociation: { prisonId: string }): boolean {
-  return prisonerOrNonAssociation.prisonId === transferPrisonId
-}
-
-export function isOutside(prisoner: OffenderSearchResult): prisoner is OffenderSearchResultOut
-export function isOutside(prisonerOrNonAssociation: { prisonId: string }): boolean
-export function isOutside(prisonerOrNonAssociation: { prisonId: string }): boolean {
-  return prisonerOrNonAssociation.prisonId === outsidePrisonId
-}
-
-export function isInPrison(prisoner: OffenderSearchResult): prisoner is OffenderSearchResultIn
-export function isInPrison(prisonerOrNonAssociation: { prisonId: string }): boolean
-export function isInPrison(prisonerOrNonAssociation: { prisonId: string }): boolean {
-  return Boolean(
-    !isBeingTransferred(prisonerOrNonAssociation) &&
-      !isOutside(prisonerOrNonAssociation) &&
-      prisonerOrNonAssociation.prisonId,
-  )
-}
-
-export type OffenderSearchResults = {
-  content: OffenderSearchResult[]
-  totalElements: number
-}
-
-export const sortOptions = ['lastName', 'firstName', 'prisonerNumber', 'cellLocation'] as const
-export type Sort = (typeof sortOptions)[number]
-
-export const orderOptions = ['ASC', 'DESC'] as const
-export type Order = (typeof orderOptions)[number]
-
 export class OffenderSearchClient extends RestClient {
-  static readonly PAGE_SIZE = 20
-
   constructor(token: string) {
     super('Offender Search API', config.apis.offenderSearchApi, token)
   }

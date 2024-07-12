544de3c2119e53624448ba5aa596a89954be54fa
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-07-12 13:17:40 +0100

    Add hash-based cache busting for client side bundles (hmpps-template-typescript#377)

--- esbuild/assets.config.js
+++ esbuild/assets.config.js
@@ -3,6 +3,7 @@ const path = require('node:path')
 const esbuild = require('esbuild')
 const { clean } = require('esbuild-plugin-clean')
 const { copy } = require('esbuild-plugin-copy')
+const manifestPlugin = require('esbuild-plugin-manifest')
 const { sassPlugin } = require('esbuild-sass-plugin')
 const { glob } = require('glob')
 
@@ -30,7 +31,7 @@ const buildAssets = buildConfig => {
   return esbuild.build({
     entryPoints: buildConfig.assets.entryPoints,
     outdir: buildConfig.assets.outDir,
-    entryNames: '[ext]/app',
+    entryNames: '[ext]/app.[hash]',
     minify: buildConfig.isProduction,
     sourcemap: !buildConfig.isProduction,
     platform: 'browser',
@@ -40,6 +41,10 @@ const buildAssets = buildConfig => {
     plugins: [
       clean({
         patterns: glob.sync(buildConfig.assets.clear),
+      }),
+      manifestPlugin({
+        generate: entries =>
+          Object.fromEntries(Object.entries(entries).map(paths => paths.map(p => p.replace(/^dist\//, '/')))),
       }),
       sassPlugin({
         quietDeps: true,

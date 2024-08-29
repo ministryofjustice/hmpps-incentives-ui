fdac4604b954d30199d3550b90da6e62d8515f70
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-08-29 11:30:30 +0100

    Copy typescript settings from IRS so that all the team’s projects match

--- server/@types/index.d.ts
+++ server/@types/index.d.ts
@@ -1,13 +1,18 @@
-/**
- * Replaces all Date types with string.
- * Needed because api responses over-the-wire are JSON and therefore encode dates as plain strings.
- */
-type DatesAsStrings<T> = {
-  [k in keyof T]: T[k] extends Array<infer U>
-    ? Array<DatesAsStrings<U>>
-    : T[k] extends Date
-      ? string
-      : T[k] extends object
-        ? DatesAsStrings<T[k]>
-        : T[k]
+export default {}
+
+export declare global {
+  /**
+   * Replaces nested Date types with string.
+   * Needed because api responses over-the-wire are JSON and therefore encode dates as plain strings.
+   * NB: arrays of Date are not supported
+   */
+  type DatesAsStrings<T> = {
+    [k in keyof T]: T[k] extends Array<infer U>
+      ? Array<DatesAsStrings<U>>
+      : T[k] extends Date
+        ? string
+        : T[k] extends object
+          ? DatesAsStrings<T[k]>
+          : T[k]
+  }
 }

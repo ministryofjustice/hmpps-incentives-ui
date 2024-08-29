5435f546fab3e9a6139415803bf0c233959b5384
ushkarev <ushkarev@users.noreply.github.com>
2024-08-29 10:54:13 +0100

    INC-1335: Add (incomplete) wiremock stub mapping type (#826)
    Add (incomplete) wiremock stub mapping type

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -2,8 +2,28 @@ import superagent, { type SuperAgentRequest, type Response } from 'superagent'
 
 const url = 'http://localhost:9091/__admin'
 
-export const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
-  superagent.post(`${url}/mappings`).send(mapping)
+/**
+ * Incomplete definition when creating a new stub mapping
+ * https://wiremock.org/docs/standalone/admin-api-reference/#tag/Stub-Mappings/operation/createNewStubMapping
+ */
+export interface Mapping {
+  request?: Partial<
+    { method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE' } & (
+      | { url: string }
+      | { urlPath: string }
+      | { urlPathPattern: string }
+      | { urlPattern: string }
+    )
+  >
+  response?: Partial<
+    {
+      status: number
+      headers: Record<string, string>
+    } & ({ jsonBody: unknown } | { body: string } | { base64Body: string })
+  >
+}
+
+export const stubFor = (mapping: Mapping): SuperAgentRequest => superagent.post(`${url}/mappings`).send(mapping)
 
 export const getMatchingRequests = (body: string | object) => superagent.post(`${url}/requests/find`).send(body)
 

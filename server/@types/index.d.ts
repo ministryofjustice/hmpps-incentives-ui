67b4fbd2127e2d0b4bc94641e048f6a7ef8ef50c
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-09-11 16:43:21 +0100

    Improve `DatesAsStrings` type converter

--- server/@types/index.d.ts
+++ server/@types/index.d.ts
@@ -4,15 +4,14 @@ export declare global {
   /**
    * Replaces nested Date types with string.
    * Needed because api responses over-the-wire are JSON and therefore encode dates as plain strings.
-   * NB: arrays of Date are not supported
    */
-  type DatesAsStrings<T> = {
-    [k in keyof T]: T[k] extends Array<infer U>
-      ? Array<DatesAsStrings<U>>
-      : T[k] extends Date
-        ? string
-        : T[k] extends object
-          ? DatesAsStrings<T[k]>
-          : T[k]
-  }
+  type DatesAsStrings<T> = T extends Date | null
+    ? string | null
+    : T extends Date
+      ? string
+      : T extends Array<infer U>
+        ? Array<DatesAsStrings<U>>
+        : T extends object
+          ? { [k in keyof T]: DatesAsStrings<T[k]> }
+          : T
 }

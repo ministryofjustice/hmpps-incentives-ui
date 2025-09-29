06963406f09822a98e42d06ee1b3888c8f020073
Jon Brighton <brightonsbox@hotmail.com>
2025-09-29 14:15:06 +0100

    CDPS-1632: Removing pointless session middleware (#593)
    * CDPS-1632: Removing pointless session middleware
    
    * CDPS-1632: Addressing comment

--- server/@types/express/index.d.ts
+++ server/@types/express/index.d.ts
@@ -6,7 +6,6 @@ declare module 'express-session' {
   // Declare that the session will potentially contain these additional fields
   interface SessionData {
     returnTo: string
-    nowInMinutes: number
   }
 }
 

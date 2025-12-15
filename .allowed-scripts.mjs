d665b3f24cc0b52e9c1225c41f88dfb624de792c
Aldo Giambelluca <xoen@users.noreply.github.com>
2025-12-15 14:08:29 +0000

    IR-1354: Moved to Node.js 24 (#1009)
    * Losen dependencies versions
    
    No point in being over specific in the `package.json`
    file.
    
    * Use `npm run setup` in `rebuild` script
    
    * Switch engine to Node.js 24, and be strict about it
    
    No dependencies for this project are requiring
    Node.js 22 or older.
    
    * Updated types for Node.js 24
    
    * Updated non-major dependencies
    
    * Running application to use Node.js 24
    
    * Fix typecheck failing on integration tests
    
    * More updates to non-major dependencies

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,13 +3,13 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Installs the Cypress binary used for integration testing
-    'node_modules/cypress@15.7.0': 'ALLOW',
+    'node_modules/cypress@15.7.1': 'ALLOW',
     // Needed by esbuild for watching files during development
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     // Provides native integration, supporting ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@0.8.8': 'ALLOW',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
-    'node_modules/esbuild@0.27.0': 'ALLOW',
+    'node_modules/esbuild@0.27.1': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
